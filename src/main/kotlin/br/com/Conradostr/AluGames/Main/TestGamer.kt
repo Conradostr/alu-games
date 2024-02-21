8ed38042199db279ce9ec99447bb9dcc57e287b8
Conradostr <caioconr63@gmail.com>
2024-02-20 21:28:47 -0300

    validacao de nome e email

--- src/main/kotlin/br/com/Conradostr/AluGames/Main/TestGamer.kt
+++ src/main/kotlin/br/com/Conradostr/AluGames/Main/TestGamer.kt
@@ -1,9 +1,10 @@
-import br.com.Conradostr.AluGames.Models.Game
 import br.com.Conradostr.AluGames.Models.Gamer
 
 fun main(){
-    val gamer1 = Gamer("Caio", "emailMuiloko@gmail.com")
+//    val gamer1 = Gamer("Caio", "emailMuiloko@gmail.com")
     val gamer2 = Gamer("Caio", "email@gmail.com", "02/06/2002", "Mezaft")
-    println(gamer1)
+    //println(gamer1)
+  println(gamer2)
+    gamer2.userName = "Jubisvaldo"
     println(gamer2)
 }
--- src/main/kotlin/br/com/Conradostr/AluGames/Models/Gamer.kt
+++ src/main/kotlin/br/com/Conradostr/AluGames/Models/Gamer.kt
@@ -1,13 +1,21 @@
 package br.com.Conradostr.AluGames.Models
 
+import kotlin.random.Random
+
 data class Gamer(
     var name:String,
     var email:String
     ){
     var dateOfBirth:String? = null
     var userName:String? = null
+        set(value) {
+            field = value
+            if (userName.isNullOrBlank()){
+                createIdInternal()
+            }
+        }
     var idInternal:String? = null
-
+        private set
     constructor(
         name: String,
         email: String,
@@ -16,7 +24,16 @@ data class Gamer(
         ): this(name, email){
             this.dateOfBirth = dateOfBirth
             this.userName = userName
+            createIdInternal()
         }
+
+    init {
+        if (name.isNullOrBlank()){
+            throw IllegalArgumentException("Nome em branco")
+        }
+
+        this.email = validateEmail()
+    }
 
     override fun toString(): String {
         return "Gamer(name='$name',\n" +
@@ -24,6 +41,22 @@ data class Gamer(
                 " dateOfBirth=$dateOfBirth,\n" +
                 " userName=$userName,\n" +
                 " idInternal=$idInternal)"
+    }
+
+    fun createIdInternal(){
+        val number = Random.nextInt(10000)
+        val tag = String.format("%04d", number)
+        idInternal = "$userName#$tag"
+    }
+
+    fun validateEmail(): String{
+        val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+        if (regex.matches(email)){
+            return email
+        }else{
+            throw IllegalArgumentException("Email invalid")
+        }
+
     }
 
 
